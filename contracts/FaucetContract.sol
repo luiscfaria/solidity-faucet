49c65d02de61f6d9afc9523cd94693398bfaed0f
Luis <luiscarlossfaria@gmail.com>
2022-10-11 20:32:49 -0300

    added funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,6 +2,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint256 public funds = 1000;
+    address[] public funders;
+
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
 }
